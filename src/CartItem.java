70f3593938603e2914083a00cd4323641abeaae0
SakuKikuchi <s.kikuchi.sys25@morijyobi.ac.jp>
2026-06-02 10:04:37 +0900

    作成

--- src/CartItem.java
+++ src/CartItem.java
@@ -1,2 +1,48 @@
 public class CartItem {
+    private int id;
+    private String name;
+    private int price;
+    private int quantity;
+
+    public CartItem(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = 1;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,36 @@
 public class Item {
+    private int id;
+    private String name;
+    private int price;
+
+    public Item(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
