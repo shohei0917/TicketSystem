e7b314c8481ab35861ff8a633ae41c7be81c9ee2
SakuKikuchi <s.kikuchi.sys25@morijyobi.ac.jp>
2026-06-10 14:37:31 +0900

    CartTest作成

--- test/CartTest.java
+++ test/CartTest.java
@@ -1,39 +1,60 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CartTest {
+
     @Test
-    void addItem() {
+    void addItemTest1() {
         Cart cart = new Cart();
-        Item item = new Item(2, "醤油ラーメン", 780);
+        Item item = new Item(1, "醤油ラーメン", 780);
 
         cart.addItem(item);
 
-        assertEquals(1, cart.getCartItems().size());
-        assertEquals("醤油ラーメン", cart.getCartItems().get(0).getName());
-        assertEquals(1, cart.getCartItems().get(0).getQuantity());
-
+        Assertions.assertEquals(1, cart.getCartItems().size());
+        Assertions.assertEquals("醤油ラーメン", cart.getCartItems().get(0).getName());
+        Assertions.assertEquals(1, cart.getCartItems().get(0).getQuantity());
     }
+
     @Test
-    void getCartItem() {
+    void addItemTest2() {
+        Cart cart = new Cart();
+        Item item = new Item(1, "醤油ラーメン", 780);
+
+        cart.addItem(item);
+        cart.addItem(item);
+
+        Assertions.assertEquals(1, cart.getCartItems().size());
+        Assertions.assertEquals(2, cart.getCartItems().get(0).getQuantity());
+    }
+
+    @Test
+    void getCartItemsTest() {
         Cart cart = new Cart();
 
-        cart.addItem(new Item(1, "特製ラーメン", 1000));
-        cart.addItem(new Item(2, "醤油ラーメン", 780));
+        Item item1 = new Item(1, "醤油ラーメン", 780);
+        Item item2 = new Item(2, "味噌ラーメン", 850);
 
-        assertEquals(2, cart.getCartItems().size());
+        cart.addItem(item1);
+        cart.addItem(item2);
 
+        Assertions.assertEquals(2, cart.getCartItems().size());
     }
 
     @Test
     void getTotalPrice() {
         Cart cart = new Cart();
 
-        cart.addItem(new Item(1, "特製ラーメン", 1000));
-        cart.addItem(new Item(2, "醤油ラーメン", 780));
-        cart.addItem(new Item(1, "特製ラーメン", 1000));
+        Item item1 = new Item(1, "醤油ラーメン", 780);
+        Item item2 = new Item(2, "味噌ラーメン", 850);
 
-        assertEquals(2780, cart.getTotalPrice());
+        cart.addItem(item1);
+        cart.addItem(item2);
+        cart.addItem(item2); // 味噌ラーメン2個
+
+        // 780 + 850*2 = 2480
+        Assertions.assertEquals(2480, cart.getTotalPrice());
     }
+
 }
